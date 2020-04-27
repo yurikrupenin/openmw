7886e11c2d08a44e4b11f1a50fa2c55d3cabae38
Yuri Krupenin <yuri.krupenin@gmail.com>
2020-04-28 01:13:09 +0300

    another shader file update

--- files/shaders/deferred_pass2_vertex.glsl
+++ files/shaders/deferred_pass2_vertex.glsl
@@ -22,25 +22,25 @@ out vec3 b_worldspace;
 
 void main()
 {
-    // Model space * Model matrix = World space
-    // World space * View matrix = Camera (eye) space
-    // Camera (eye) space * Projection matrix = Screen space
-    // We simply translate vertex from Model space to Screen space.
     gl_Position = osg_ModelViewProjectionMatrix * vec4(aPos, 1.0);
+
     // Pass the texture coordinate further to the fragment shader.
     texCoords = aTexCoords.xy;
-    // osg_ModelViewMatrix, osg_NormalMatrix, etc. are in Camera(eye) space
-    // Discussion: http://forum.openscenegraph.org/viewtopic.php?p=44257#44257
-    // Camera (eye) space * inverse View matrix = World space
+
     mat4 modelMatrix = osg_ViewMatrixInverse * osg_ModelViewMatrix;
     mat3 modelMatrix3x3 = mat3(modelMatrix);
+
+
     // Convert everything to World space.
     // Position.
     pos_worldspace = modelMatrix * vec4(aPos, 1.0);
+
     // Normal.
     n_worldspace   = modelMatrix3x3 * osg_Normal;
+
     // Tangent.
     t_worldspace   = modelMatrix3x3 * aTangentSpace.xyz;
+    
     // Bitangent / binormal.
     b_worldspace   = cross(n_worldspace, t_worldspace);
 }
